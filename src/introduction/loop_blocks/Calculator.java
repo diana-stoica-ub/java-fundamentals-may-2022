1a802b192520ccd496feb8743b25d4e32192a230
dianabalutoiu <dianabalutoiu@gmail.com>
2022-05-12 09:14:41 +0300

    Calculator

--- src/introduction/loop_blocks/Calculator.java
+++ src/introduction/loop_blocks/Calculator.java
@@ -12,9 +12,15 @@ public class Calculator {
     //Se citeste un operator (+, -, /, *) si al doilea numar pozitiv
     //Daca se citeste "X" -> se inchide aplicatia
     //Daca se citeste "C" -> clear, introducem din nou primul numar
+
+    private static final char ADD = '+';
+    private static final char SUBSTRACT = '-';
+    private static final char MULTIPLY = '*';
+    private static final char DIVIDE = '/';
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number1 = scanner.nextInt();
+        int number1 = readNumber(scanner);
         char operator;
         int number2;
 
@@ -24,48 +30,56 @@ public class Calculator {
                 if ("X".equals(line)) {
                     return;
                 } else if ("C".equals(line)) {
-                    number1 = scanner.nextInt();
+                    number1 = readNumber(scanner);
                 } else {
                     operator = line.charAt(0);
                     validateOperator(operator);
 
-                    number2 = scanner.nextInt();
+                    number2 = readNumber(scanner);
                     validateForDivisionByZero(operator, number2);
 
                     number1 = displayResult(number1, number2, operator);
                 }
-
             } catch (InvalidOperatorException e) {
                 System.out.println("Operator not valid, please use: + , - , / , *");
             } catch (DivisionByZeroException e) {
                 System.out.println(e.getMessage());
-            } catch (InputMismatchException e) {
+            }
+        }
+    }
+
+    private static int readNumber(Scanner scanner) {
+        while (true) {
+            try {
+                String line = scanner.next();
+                return Integer.parseInt(line);
+
+            } catch (NumberFormatException numberFormatException) {
                 System.out.println("Please insert an int");
             }
-
         }
     }
 
     private static void validateForDivisionByZero(char operator, int number) throws DivisionByZeroException {
-        if (operator == '/' && number == 0) {
+        if (operator == DIVIDE && number == 0) {
             throw new DivisionByZeroException("Division by zero! Please insert a different operator or a number != 0");
         }
     }
     private static void validateOperator(char operator) throws InvalidOperatorException {
-        if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+        if (operator != ADD && operator != SUBSTRACT && operator != MULTIPLY && operator != DIVIDE) {
             throw new InvalidOperatorException();
         }
     }
 
     private static int displayResult(int a, int b, char operator) {
         int result = 0;
-        if (operator == '+') {
+        if (operator == ADD) {
              result = a + b;
-        } else if (operator == '-') {
+        } else if (operator == SUBSTRACT) {
             result = a - b;
-        } else if (operator == '*') {
+        } else if (operator == MULTIPLY) {
             result = a * b;
-        } else if (operator == '/') {
+        } else if (operator == DIVIDE) {
             result = a / b;
         } else {
             System.out.println("Operator not valid");
